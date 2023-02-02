b08abc9518d5ee7cb161bb0fb3542df9e4d3c477
Carlos Nascimento <acntonycar62@gmail.com>
2023-02-01 23:00:10 -0300

    first commit

---
+++ src/data/Words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braco", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo"],
+  };
